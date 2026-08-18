9b756b3835ee7f9ffb40a12a63e6077dadc4f108
Centaurioun <86323645+Centaurioun@users.noreply.github.com>
2026-08-18 14:27:40 +0300

    inspect(f5-017): narrow diagnostic to P53 token sequence

--- work/inspect_f5_017.py
+++ work/inspect_f5_017.py
@@ -6,7 +6,6 @@ import hashlib, shutil, sys
 
 W='http://schemas.openxmlformats.org/wordprocessingml/2006/main'; NS={'w':W}
 EXPECTED='cc3d906b77ae5325b6bcb9b5e458b1af30ef37191c5ee956455613161bd693da'
-TARGET='Bu döneme ilişkin rivâyetler, vahyin yazıya geçirilmesi hususunda çok daha sistemli ve titiz bir uygulamanın bulunduğunu göstermektedir.'
 
 def sha256(p): return hashlib.sha256(Path(p).read_bytes()).hexdigest()
 def text(p): return ''.join(p.xpath('.//w:t/text()',namespaces=NS))
@@ -19,16 +18,10 @@ def main(src,out):
         d=etree.fromstring(z.read('word/document.xml'))
         ps=d.xpath('.//w:body/w:p',namespaces=NS)
         if len(ps)!=674: raise RuntimeError(f'body count {len(ps)}')
-        print(f'SHA256={got}')
-        print(f'BODY_PARAGRAPHS={len(ps)}')
-        hits=[]
-        for i in range(50,56):
-            t=text(ps[i])
-            if TARGET in t: hits.append(i)
-            fns=ps[i].xpath('.//w:footnoteReference/@w:id',namespaces=NS)
-            print(f'P{i}\tFN={fns}\t{t}')
-        print('TARGET_HITS='+repr(hits))
         p=ps[53]
+        print(f'SHA256={got}')
+        print('P53_TEXT='+text(p))
+        print('P53_FNS='+repr(p.xpath('.//w:footnoteReference/@w:id',namespaces=NS)))
         print('P53_SEQUENCE_BEGIN')
         for j,n in enumerate(p.xpath('.//*[self::w:t or self::w:footnoteReference]',namespaces=NS)):
             if n.tag==f'{{{W}}}t': print(f'{j:02d}\tTEXT\t{n.text or ""}')
